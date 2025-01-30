fa596d3e0bb0fccb12760657260e264e6aff23a1
Santhosh Deenadayalan <30692153+santhoshdeena@users.noreply.github.com>
2025-01-30 21:20:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,249 @@
-// Set the wedding date (YYYY, MM-1, DD)
-const weddingDate = new Date(2025, 2, 09); // February is month 1 (0-indexed)
 
-function updateCountdown() {
-  const now = new Date();
-  const timeDifference = weddingDate - now;
+	
+(function ($) {
 
-  // Calculate days, hours, minutes, and seconds
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    "use strict";
 
-  // Update the countdown display
-  document.getElementById('days').textContent = String(days).padStart(2, '0');
-  document.getElementById('hours').textContent = String(hours).padStart(2, '0');
-  document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
-  document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
+	// JQUERY LIGHT BOX
+	
+	if ( $.isFunction($.fn.fluidbox) ) {
+		$('a').fluidbox();
+	}
 
-  // If the wedding date has passed
-  if (timeDifference < 0) {
-    clearInterval(countdownInterval);
-    document.querySelector('.countdown').innerHTML = "<p>We're married! Thank you for celebrating with us!</p>";
-  }
+	
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	// COUNTDOWN TIME 
+	
+	countdownTime();
+	
+	
+	$('[data-nav-menu]').on('click', function(event){
+		
+		var $this = $(this),
+			visibleHeadArea = $this.data('nav-menu');
+		
+		$(visibleHeadArea).toggleClass('visible');
+		
+	});
+	
+	
+	var winWidth = $(window).width();
+	dropdownMenu(winWidth);
+	
+	$(window).on('resize', function(){
+		dropdownMenu(winWidth);
+		
+	});
+	
+	// Circular Progress Bar
+	
+	var isAnimated = false;
+	
+	
+})(jQuery);
+
+
+
+function countdownTime(){
+	
+	if(isExists('#clock')){
+		$('#clock').countdown('2018/01/01', function(event){
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="time-sec"><span class="title">%D</span> days </div>'
+				+ '<div class="time-sec"><span class="title">%H</span> hours </div>'
+				+ '<div class="time-sec"><span class="title">%M</span> minutes </div>'
+				+ '<div class="time-sec"><span class="title">%S</span> seconds </div>'));
+		});
+	}
+}
+function dropdownMenu(winWidth){
+	
+	if(winWidth > 767){
+		
+		$('.main-menu li.drop-down').on('mouseover', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.addClass('mouseover');
+			
+		}).on('mouseleave', function(){
+			var $this = $(this),
+				menuAnchor = $this.children('a');
+				
+			menuAnchor.removeClass('mouseover');
+		});
+		
+	}else{
+		
+		$('.main-menu li.drop-down > a').on('click', function(){
+			
+			if($(this).attr('href') == '#') return false;
+			if($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
+			else{ $(this).addClass('mouseover'); }
+			return false;
+		});
+	}
+	
 }
 
-// Update the countdown every second
-const countdownInterval = setInterval(updateCountdown, 1000);
-updateCountdown(); // Initial call
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
+
+function initMap() {
+
+	// Create a new StyledMapType object, passing it an array of styles,
+	// and the name to be displayed on the map type control.
+	var styledMapType = new google.maps.StyledMapType(
+	[
+		{
+			"featureType": "administrative",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "administrative.province",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": 65
+				},
+				{
+					"visibility": "on"
+				}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"lightness": "50"
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": "-100"
+				}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "30"
+				}
+			]
+		},
+		{
+			"featureType": "road.local",
+			"elementType": "all",
+			"stylers": [
+				{
+					"lightness": "40"
+				}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+				{
+					"saturation": -100
+				},
+				{
+					"visibility": "simplified"
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "geometry",
+			"stylers": [
+				{
+					"hue": "#ffff00"
+				},
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -97
+				}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "labels",
+			"stylers": [
+				{
+					"lightness": -25
+				},
+				{
+					"saturation": -100
+				}
+			]
+		}
+	],
+		{name: 'Styled Map'});
+
+	// Create a map object, and include the MapTypeId to add
+	// to the map type control.
+
+	var uluru = {lat: 56.946285, lng: 24.105078};
+    var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 4,
+		center: uluru
+    });
+		
+	var image = 'images/google-marker.png';
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+		icon: image
+	});
+	//Associate the styled map with the MapTypeId and set it to display.
+	map.mapTypes.set('styled_map', styledMapType);
+	map.setMapTypeId('styled_map');
+}
